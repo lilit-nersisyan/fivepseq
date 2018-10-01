b3abd5ccdcb1efc7ef616988cd1afa84057b074e
Lilit Nersisyan <lilit@rackham4.uppmax.uu.se>
2018-10-01 15:43:07 +0200

    iniital commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fivepseq',
+      version='0.1',
+      description='A package for analysis of 5pseq datasets',
+      url='http://github.com/lilit-nersisyan/fivepseq',
+      author='Lilit Nersisyan',
+      author_email='lilit.nersisyan@ki.se',
+      license='BSD 3-Clause',
+      packages=['fivepseq'],
+      zip_safe=False)
